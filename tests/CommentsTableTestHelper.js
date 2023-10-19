6420e7607ca3294dbeedc16a2c9125d4ef7cea57
azarnuzy <azarnuzy@gmail.com>
2023-10-19 10:50:23 +0700

    improvement: add comment table test helper

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,33 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool')
+
+const CommentTableTestHelper = {
+  async addComment({
+    id = 'comment-123',
+    content = 'A Comment',
+    owner = 'user-123',
+    thread_id = 'thread-123',
+    is_delete = false,
+  }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, content, owner, thread_id, is_delete, date],
+    }
+    await pool.query(query)
+  },
+  async getCommentById(id) {
+    const query = {
+      text: `SELECT * FROM comments WHERE id = '${id}'`,
+      values: [id],
+    }
+
+    const result = await pool.query(query)
+
+    return result.rows
+  },
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1')
+  },
+}
+
+module.exports = CommentTableTestHelper
