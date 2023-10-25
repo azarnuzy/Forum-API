6d87067791ad94ef43fb33075386c9575e0e6214
azarnuzy <azarnuzy@gmail.com>
2023-10-25 12:19:42 +0700

    improvement: add getCommentByThreadId repository

--- src/Domains/comments/_test/CommentRepository.test.js
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -16,5 +16,9 @@ describe('CommentRepository interface', () => {
     await expect(commentRepository.deleteCommentById('')).rejects.toThrowError(
       'COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED'
     )
+
+    await expect(
+      commentRepository.getCommentsByThreadId('')
+    ).rejects.toThrowError('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
   })
 })
