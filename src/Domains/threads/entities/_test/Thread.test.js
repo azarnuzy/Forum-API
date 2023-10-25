70bcfccf6834a3a0da76160ecfc6a4f585d623d0
azarnuzy <azarnuzy@gmail.com>
2023-10-25 11:54:29 +0700

    improvement: add thread entities domain

---
+++ src/Domains/threads/entities/_test/Thread.test.js
@@ -0,0 +1,52 @@
+const Thread = require('../Thread')
+
+describe('a Thread entity', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'thread-123',
+    }
+
+    // Action & Assert
+    expect(() => new Thread(payload)).toThrowError(
+      'GET_THREAD.NOT_CONTAIN_NEEDED_PROPERTY'
+    )
+  })
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      title: true,
+      body: 123,
+      date: Date.now(),
+      username: 'dicoding',
+    }
+
+    // Action & Assert
+    expect(() => new Thread(payload)).toThrowError(
+      'GET_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    )
+  })
+
+  it('should create Thread object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'thread-123',
+      title: 'title thread',
+      body: 'body thread',
+      date: Date.now(),
+      username: 'dicoding',
+    }
+
+    // Action
+    const { id, title, body, date, username } = new Thread(payload)
+
+    // Assert
+    expect(id).toEqual(payload.id)
+    expect(title).toEqual(payload.title)
+    expect(body).toEqual(payload.body)
+    expect(date).toEqual(payload.date)
+    expect(username).toEqual(payload.username)
+  })
+})
